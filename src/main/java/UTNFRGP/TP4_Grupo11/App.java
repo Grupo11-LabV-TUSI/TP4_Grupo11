7b2fac3844ac811e644a901d58531904e0aa796b
Joselo Esteban Orellano <mobilelegendsbang2020bang@gmail.com>
2024-05-14 18:29:57 -0300

    #US-1 #TK-2 Realizada con exito

--- src/main/java/UTNFRGP/TP4_Grupo11/App.java
+++ src/main/java/UTNFRGP/TP4_Grupo11/App.java
@@ -1,5 +1,12 @@
 package UTNFRGP.TP4_Grupo11;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import dao.DaoHibernatePaciente;
+import entidad.Paciente;
+import excepciones.PK_Paciente_Repetida;
+
 /**
  * Hello world!
  *
@@ -9,5 +16,29 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Paciente paciente = new Paciente(
+				        		12345678,
+				        		"pacienteUno",
+				        		"apellidoUno",
+				        		"paciente1@email.com",
+				        		"telefono1",
+				        		LocalDate.of(1900,1,1),
+				        		"direccion1",
+				        		"localidad1",
+				        		"provincia1"
+				        	);
+        try {
+        	DaoHibernatePaciente.crear(paciente);
+        } catch (PK_Paciente_Repetida e) {
+        	e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			paciente.setFecha_nacimiento(LocalDate.of(2000, 1, 1));
+			DaoHibernatePaciente.actualizar(paciente);
+			System.out.println(DaoHibernatePaciente.leer(paciente.getDni()));
+			DaoHibernatePaciente.borrar(paciente);
+			System.out.println(DaoHibernatePaciente.leerTodos());
+		}
     }
 }
---
+++ src/main/java/entidad/Paciente.java
@@ -0,0 +1,140 @@
+package entidad;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Paciente")
+public class Paciente implements Serializable {
+	// Implementar serializable
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name="dni")
+	private int dni;
+	
+	@Column(name="nombre")
+	private String nombre;
+	
+	@Column(name="apellido")
+	private String apellido;
+	
+	@Column(name="email")
+	private String emial;
+	
+	@Column(name="telefono")
+	private String telefono;
+	
+	@Column(name="fecha_nacimiento")
+	private LocalDate fecha_nacimiento;
+	
+	@Column(name="direccion")
+	private String direccion;
+	
+	@Column(name="localidad")
+	private String localidad;
+	
+	@Column(name="provincia")
+	private String provincia;
+	
+	// Constructor vacio requerido por Hibernate
+	public Paciente() {}
+
+	public Paciente(int dni, String nombre, String apellido, String emial, String telefono, LocalDate fecha_nacimiento,
+			String direccion, String localidad, String provincia) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.emial = emial;
+		this.telefono = telefono;
+		this.fecha_nacimiento = fecha_nacimiento;
+		this.direccion = direccion;
+		this.localidad = localidad;
+		this.provincia = provincia;
+	}
+
+	// Geters y Seters
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmial() {
+		return emial;
+	}
+
+	public void setEmial(String emial) {
+		this.emial = emial;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public LocalDate getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+
+	public void setFecha_nacimiento(LocalDate fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	@Override
+	public String toString() {
+		return "Paciente [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", emial=" + emial
+				+ ", telefono=" + telefono + ", fecha_nacimiento=" + fecha_nacimiento + ", direccion=" + direccion
+				+ ", localidad=" + localidad + ", provincia=" + provincia + "]";
+	}
+
+}
