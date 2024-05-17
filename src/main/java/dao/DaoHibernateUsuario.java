bc684b96bc407fee7c64e14b7ce15d5c548619f8
Silva-Carlos-Andres <silva_carlos_andres@hotmail.com>
2024-05-17 00:38:17 -0300

    Clase Usuario
    Version 4

---
+++ src/main/java/dao/DaoHibernateUsuario.java
@@ -0,0 +1,54 @@
+package dao;
+import entidad.Usuario;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DaoHibernateUsuario {
+	private SessionFactory sessionFactory;
+
+	public void crearUsuario(Usuario usuario) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(usuario);
+        session.getTransaction().commit();
+        session.close();
+    }
+    public Usuario obtenerUsuarioPorId(Long id) {
+        Session session = sessionFactory.openSession();
+        Usuario usuario = (Usuario) session.get(Usuario.class, id);
+        session.close();
+        return usuario;
+    }
+
+    public void actualizarUsuario(Usuario usuario) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.update(usuario);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void borrarUsuario(Long id) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Usuario usuario = (Usuario) session.get(Usuario.class, id);
+        if (usuario != null) {
+            session.delete(usuario);
+        }
+        session.getTransaction().commit();
+        session.close();
+    }
+
+
+
+
+    // Cerrar el SessionFactory
+    public void cerrar() {
+        sessionFactory.close();
+    }
+    
+
+}
---
+++ src/main/java/entidad/Usuario.java
@@ -0,0 +1,62 @@
+package entidad;
+import java.io.Serializable;
+import java.sql.Date;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "usuario", nullable = false, unique = true)
+    private String Usuario;
+
+    @Column(name = "contrasena", nullable = false)
+    private String contrasena;
+
+    // Getters y setters
+
+    // Constructor
+    public Usuario() {}
+
+    public Usuario(String Usuario, String contrasena) {
+        this.Usuario = Usuario;
+        this.contrasena = contrasena;
+    }
+
+    // Getters y setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(String Usuario) {
+        this.Usuario = Usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContraseña(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
